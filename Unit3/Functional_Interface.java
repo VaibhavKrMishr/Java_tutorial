67a9a50e09ad10f1f9d6e4e1338b57bc52b9f983
VaibhavKrMishr <mishra.vaibhavkumar9533@gmail.com>
2026-03-25 11:18:09 +0530

    Unit 4

--- Unit3/Functional_Interface.java
+++ Unit3/Functional_Interface.java
@@ -1,11 +1,29 @@
-interface MyInterface{
-    void display();
+// interface MyInterface{
+//     void display();
+//     void show();
+// }
+
+//For one interface only 
+@FunctionalInterface 
+interface MyInterface2{
+    void dekha();
+    default void show1(){
+        System.out.println("Hello Default");
+    }
+   
 }
 
-class Myclass implements MyInterface{
+class Myclass implements MyInterface2{
     public void display(){
         System.out.println("Hello");
     }
+    public void show(){
+        System.out.println("Hello Adarsh");
+    }
+    public void dekha(){
+        System.out.println("Dekh le ");
+    }
+    
 }
 
 class Functional_Interface {
@@ -13,5 +31,8 @@ class Functional_Interface {
     public static void main(String[] args) {
         Myclass obj = new Myclass();
         obj.display();
+        obj.show();
+        obj.dekha();
+        obj.show1();
     }
 }
